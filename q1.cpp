ea937912e00f7c2820a92926787c3e08f6f22917
Mantu Kumar <118504139+mantu01714@users.noreply.github.com>
2024-12-24 18:24:43 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+#include <climits>
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> dataStack;
+    stack<int> minStack;
+
+public:
+    MinStack() {
+        minStack.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        dataStack.push(val);
+        minStack.push(min(val, minStack.top()));
+    }
+    
+    void pop() {
+        dataStack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return dataStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
+
+int main() {
+    MinStack minStack;
+
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+
+    cout << minStack.getMin() << endl;
+    minStack.pop();
+    cout << minStack.top() << endl;   
+    cout << minStack.getMin() << endl;
+
+    return 0;
+}
